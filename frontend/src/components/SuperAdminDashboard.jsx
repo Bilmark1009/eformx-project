ffeeacd3f6a4cb885932adb11072d5137dc50227
Bilmark1009 <sinangotebilmark77@gmail.com>
2026-02-11 10:17:16 +0800

    cleanedup final unused imports attempting to rerun checks

--- frontend/src/components/SuperAdminDashboard.jsx
+++ frontend/src/components/SuperAdminDashboard.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import "../styles/SuperAdminDashboard.css";
 import { FaBell, FaPlus, FaEdit, FaTrash, FaUserEdit, FaCamera, FaUsers, FaFileAlt, FaCheckCircle, FaUserShield } from "react-icons/fa";
 import CreateAccountModal from "./CreateAccountModal";
